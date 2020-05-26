252a79847f4791f8b2cb0d14fbe8eec2f5408432
shogosakai <shogosakai73@gmail.com>
2020-05-26 16:03:01 +0900

    make "About" page

--- myblogapp/myblogapp/urls.py
+++ myblogapp/myblogapp/urls.py
@@ -20,8 +20,9 @@ from django.conf import settings
 from posts import views
 
 urlpatterns = [
-    # url(r'', include('posts.urls')),
+    url(r'', include('posts.urls')),
     url(r'^posts/', include('posts.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^about/', include('posts.urls')),
     url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_detail, name="post_detail")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
